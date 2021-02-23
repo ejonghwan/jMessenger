c338aba184477d344dffe5e9e9a70bd4eb5054b1
ejonghwan <sun87-1@daum.net>
2021-02-24 02:04:45 +0900

    1.0

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,8 @@
 import React from 'react'
+import AppRouter from './Router';
 
 function App() {
-  return (
-    <div className="App">
-      asd
-    </div>
-  );
+  return <AppRouter />
 }
 
 export default App;
---
+++ src/routes/Auth.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Auth = () => {
+    return (
+        <div>
+            ayth
+        </div>
+    );
+};
+
+export default Auth;
