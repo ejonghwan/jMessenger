c316d9c29d6dee08ddd103392c988accc7a74602
ejonghwan <sun87-1@daum.net>
2021-02-26 01:43:50 +0900

    2.1

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,9 +1,36 @@
-import React from 'react';
+import { dbService } from 'fbase';
+import React, { useState } from 'react';
 
 const Home = ({  }) => {
+
+    const [jme, setJme] = useState('')
+    const onSubmit = e => {
+        e.preventDefault();
+        dbService.collection("jme").add({
+            jonghwan: 'goodman',
+            createdAt: Date.now()
+        })
+        console.log('asdasd')
+    }
+
+    const onChange = e => {
+        const { target: {value} } = e
+        setJme(value)
+    }
+
     return (
         <div>
-            home
+            <form onSubmit={onSubmit}>
+                <input 
+                    type="text" 
+                    value={jme} 
+                    onChange={onChange} 
+                    placeholder="what's on your mind" 
+                    maxLength={120} 
+                    />
+                <input type="submit" value="jmessegn" />
+            </form>
+          
         </div>
     );
 };
